4373ddded23282a04ba5dadd6947290472e858c7
fernandesraquel <raquel.lima.072@ufrn.edu.br>
2023-07-20 17:43:02 -0300

    add views editar, excluir produto

--- apps/stock/urls.py
+++ apps/stock/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     
     path('criar/produto', views.CriarProduto.as_view(), name='criar_produto'),
     path('listar/produtos', views.ListarProdutos.as_view(), name='listar_produtos'),
+    path('editar/<str:produto_id>/produto', views.EditarProduto.as_view(), name='editar_produto'),
+    path('excluir/<str:produto_id>/produto', views.ExcluirProduto.as_view(), name='excluir_produto'),
 ]
